0ac453da706fb971ea1aae741731c73e84e9bb6a
mehabhatt <mehabhatt21@gmail.com>
2023-10-28 11:28:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_practical_11_21012011049/MapsActivity.kt
@@ -0,0 +1,57 @@
+package com.example.mad_practical_11_21012011049
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.example.mad_practical_11_21012011049.databinding.ActivityMapsBinding
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+    private var lat:Double = 0.0
+    private var log:Double = 0.0
+    private var title = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val obj = intent.getSerializableExtra("Object") as Person
+        lat = obj.latitude
+        log = obj.longitude
+        title = obj.name
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        mMap.mapType = GoogleMap.MAP_TYPE_SATELLITE
+        val sydney = LatLng(lat,  log )
+        mMap.addMarker(MarkerOptions().position(sydney)
+            .title(title)
+            .snippet(title)
+            .icon(BitmapDescriptorFactory.fromResource(R.drawable.img_1))
+        )
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,16.0f))
+    }
+}
---
+++ app/src/main/java/com/example/mad_practical_11_21012011049/PersonAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.mad_practical_11_21012011049
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.button.MaterialButton
+import java.util.ArrayList
+
+class PersonAdapter(context:Context,  val Personarray : ArrayList<Person>): ArrayAdapter<Person>(context,0,Personarray) {
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val view=LayoutInflater.from(context).inflate(R.layout.contact_item, parent, false)
+        view.findViewById<TextView>(R.id.name_textView).text = Personarray[position].name
+        view.findViewById<TextView>(R.id.number_textView).text= Personarray[position].phoneNo
+        view.findViewById<TextView>(R.id.email_textView).text= Personarray[position].emailId
+        view.findViewById<TextView>(R.id.address_textView).text= Personarray[position].address
+        view.findViewById<MaterialButton>(R.id.location_btn).setOnClickListener {
+            Intent(context, MapsActivity::class.java).putExtra("Object",Personarray[position]).apply { context.startActivity(this) }
+        }
+        return view
+    }
+}
